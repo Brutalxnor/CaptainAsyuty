d43177e48c077982b3ea7f25e6e1e7cc1f931011
Brutalxnor <88873022+Brutalxnor@users.noreply.github.com>
2024-07-29 19:06:26 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -60,16 +60,30 @@
 /** @type {import('next').NextConfig} */
 
 
-const nextConfig = {}
-module.exports = nextConfig
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/socket.io/:path*',
-          destination: '/api/socket',
-        },
-      ];
-    },
-  };
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_API_URL: process.env.NEXT_PUBLIC_CLERK_API_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    MONGODB_URI: process.env.MONGODB_URI,
+    // Add other environment variables here as needed
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/socket.io/:path*',
+        destination: '/api/socket',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
+
 
