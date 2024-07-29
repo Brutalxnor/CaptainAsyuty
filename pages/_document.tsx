dce6f0ee53f0f5285fc5526e592fada64a1ba1cc
Brutalxnor <88873022+Brutalxnor@users.noreply.github.com>
2024-07-29 19:15:00 +0300

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          {/* Add other meta tags or links here as needed */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
