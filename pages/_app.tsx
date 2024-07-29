04d4f52b1192059f54a1e196604c537815bd5c5a
Brutalxnor <88873022+Brutalxnor@users.noreply.github.com>
2024-07-29 23:55:23 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,12 @@ import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+    <ClerkProvider 
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      // frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API!}
+      signInUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL!}
+      signUpUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL!}>
+
       <LanguageProvider>
         <Component {...pageProps} />
       </LanguageProvider>
